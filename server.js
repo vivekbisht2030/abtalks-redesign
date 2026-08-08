d00b23d46237cc2a7d381b58f0f985e4b659c5cd
Vivek Bisht <vivekbisht3007@gmail.com>
2026-08-08 14:29:32 +0530

    Setup project structure: added boilerplate, layouts, public folder and fixed server routes

--- server.js
+++ server.js
@@ -2,34 +2,45 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
 const PORT = 3000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+
+// Helper function to read data.json safely
+const readData = () => {
+    const filePath = path.join(__dirname, 'data.json');
+    if (!fs.existsSync(filePath)) {
+        return { users: [], currentDay: {} };
+    }
+    const jsonData = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(jsonData);
+};
 
 // 1. Landing Page ( / )
 // The first experience for a student
 app.get('/', (req, res) => {
-    // 
-    res.send('Landing Page Content Will Go Here');
+    res.render('ejs/index.ejs');
 });
 
 // 2. Student Dashboard ( /dashboard )
 // Includes current streak, today's task, progress, overall completion
 app.get('/dashboard', (req, res) => {
-    // 
-    res.send('Dashboard Data/Page Will Go Here');
+    //
 });
 
 // 3. Challenge Day ( /day/12 )
 app.get('/day/:dayNumber', (req, res) => {
-    const day = req.params.dayNumber;
-    // 
-    res.send(`Challenge Day ${day} Content Will Go Here`);
+    //
 });
 
 
